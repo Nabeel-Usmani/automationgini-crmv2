54896c44c52e78ace1324c726d8c3e614a086ddf
Nabeel Usmani <130184043+Nabeel-Usmani@users.noreply.github.com>
2026-07-13 21:33:38 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import AuthCallback from './pages/AuthCallback'
-import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Layout from './components/Layout'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth/callback" element={<AuthCallback />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Layout>{(user) => <Home user={user} />}</Layout>} />
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
